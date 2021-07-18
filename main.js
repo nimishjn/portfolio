6225a1b25d9f97d45ba0b275d98a2ca6f7d35564
nimishjn <nimishjain100701@gmail.com>
2021-07-18 14:26:30 +0530

    Skills Section

---
+++ main.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    loadSkillsPage();
+    loadProjectsPage();
+    loadResearchPage();
+}, false);
+
+function loadSkillsPage() {
+    var skills = [{name: "C", image: "./assets/c.png"},
+                  {name: "C++", image: "./assets/cpp.png"},
+                  {name: "Java", image: "./assets/java.png"},
+                  {name: "Python", image: "./assets/python.png"},
+                  {name: "HTML", image: "./assets/HTML5.png"},
+                  {name: "CSS", image: "./assets/css3.png"},
+                  {name: "JavaScript", image: "./assets/javascript.png"},
+                  {name: "Matlab", image: "./assets/matlab.png"},
+                  {name: "Git", image: "./assets/git.png"},
+                  {name: "GitHub", image: "./assets/github.png"},
+                  {name: "Discord.py", image: "./assets/discord.png"}
+                ];
+    
+    var allSkills = document.getElementById('all-skills');
+
+    for(var i = 0; i < skills.length; i++) {
+        var element = document.createElement('div');
+        element.classList.add('skill-card');
+        element.innerHTML = `
+            <p>${skills[i].name}</p>
+            <img src="${skills[i].image}" alt="${skills[i].name}">
+        `;
+        allSkills.appendChild(element);
+    }
+}
+
+function loadProjectsPage() {
+
+}
+
+function loadResearchPage() {
+
+}
